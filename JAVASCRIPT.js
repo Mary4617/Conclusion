0e08565dd7663c527a8e6533684faee27591ec16
Mary4617 <39519344+Mary4617@users.noreply.github.com>
2018-09-25 22:34:42 +0300

    Add files via upload

---
+++ JAVASCRIPT.js
@@ -0,0 +1,13 @@
+﻿function f2()
+{
+('tx1').value="lll";
+}
+function f3()
+{
+alert("Это- милая альпака. Хорошего дня!")
+}
+function f()
+{
+document.fgColor="MidnightBlue";
+document.bgColor="PeachPuff";
+}
