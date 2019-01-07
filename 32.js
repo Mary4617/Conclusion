953429c2aebfb63d3831857b8b36dd4473e0ffe8
MaryP18 <39519344+MaryP18@users.noreply.github.com>
2019-01-07 22:12:20 +0300

    Add files via upload

---
+++ 32.js
@@ -0,0 +1,32 @@
+﻿function $() 
+{ var elements = new Array(); 
+for (var i = 0; i < arguments.length; i++) 
+{ var element = arguments[i]; 
+if (typeof element == 'string') 
+element = document.getElementById(element); 
+if (arguments.length == 1) 
+return element; 
+elements.push(element); 
+} 
+return elements; }  
+
+
+function f1()
+{A=parseFloat($('tt1').value);
+ B=parseFloat($('tt2').value);
+ N=parseFloat($('tt3').value);
+                     for(i=1;i<=N;i++) 
+		          {H=(B-A)/N;}
+		      $('tt4').value=H;
+}
+
+function f2()
+{A=parseFloat($('tt1').value);
+ B=parseFloat($('tt2').value);
+ N=parseFloat($('tt3').value);
+ H=parseFloat($('tt4').value);
+                   for (i=0;i<=N;i++)
+                         {x=A+i*H;
+                          $('tt5').value=$('tt5').value+' '+x;
+}
+}
---
+++ 34.js
@@ -0,0 +1,24 @@
+﻿function $() 
+{ var elements = new Array(); 
+for (var i = 0; i < arguments.length; i++) 
+{ var element = arguments[i]; 
+if (typeof element == 'string') 
+element = document.getElementById(element); 
+if (arguments.length == 1) 
+return element; 
+elements.push(element); 
+} 
+return elements; }  
+
+
+function f1()
+{
+var N=parseInt($('tt1').value);
+var K;
+
+while(Math.pow(3,K)>N);
+{
+	K++;
+}
+$('tt2').value=K;
+}
