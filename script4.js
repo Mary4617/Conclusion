f0c3fd943abbc2a1b3128249070e3b1a126c96f8
MaryP18 <39519344+MaryP18@users.noreply.github.com>
2019-01-20 22:49:48 +0300

    Add files via upload

---
+++ script4.js
@@ -0,0 +1,30 @@
+﻿﻿function $() 
+{ var elements = new Array(); 
+for (var i = 0; i < arguments.length; i++) 
+{ var element = arguments[i]; 
+if (typeof element == 'string') 
+element = document.getElementById(element); 
+if (arguments.length == 1) 
+return element; 
+elements.push(element); 
+} 
+return elements; }
+
+function f1(x)
+{
+var gg="1234567890";
+fff="Okay";
+var mass1 = x.split('0');
+if(mass1.length==2)
+{
+for(i=5;i<x.length;i++)
+{
+if(gg.indexOf(x.charAt(i))<5)
+fff="Fail";
+}
+}
+else
+fff="Fail";
+return fff;
+
+}
